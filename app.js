cb73371f13cfd5ca212f13d1c7d8e39e77cfa788
Harsh Gandhi <63015502+Harshgandhi29@users.noreply.github.com>
2020-07-21 00:28:18 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+let data; 
+var long=0;
+var lat=0;
+let position = document.getElementById('Location')
+var mymap = L.map('mapid').setView([5,5], 1);
+var marker=L.marker([1,2]).addTo(mymap);
+const iss_image= L.icon({
+    iconUrl:"iss_2.png",iconSize:[38, 95]
+})
+
+function iss() {
+    fetch('https://api.wheretheiss.at/v1/satellites/25544')
+        .then(res => {
+            console.log('part1')
+            return res.json()
+            })
+        .then(data =>{
+    console.log('part2')
+    long = data.longitude
+    lat = data.latitude
+    lat = lat.toFixed(3);
+    long = long.toFixed(3); 
+    position.textContent =lat+" and "+ long  
+    map(lat,long);
+}
+)}
+
+function map (lat, long){
+   
+    mymap.setView([lat,long], 5);
+    mymap.removeLayer(marker);   
+    marker = L.marker([lat,long],{icon:iss_image}).addTo(mymap);
+
+}
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+  /*  id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiaGFyc2gyOSIsImEiOiJja2NwNmhrMzMwM2hiMnpwZ283eDhyajlhIn0.6BTR-982sfogi5fKSfWCmw'
+*/}).addTo(mymap);
+
+
+
+
+
+
+
+iss();
+//map(51.505,-.099);
+setInterval(iss,10000);
+
+
+
+
+
+ 
